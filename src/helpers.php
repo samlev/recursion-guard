4ec6203ec053ebcd2246d37557a373c0765b7ecc
Samuel Levy <sam@determineddevelopment.com>
2024-09-29 20:25:27 +1000

    Cleanups

--- src/helpers.php
+++ src/helpers.php
@@ -8,9 +8,11 @@ if (! function_exists('array_only')) {
     /**
      * Get a subset of the items from the given array.
      *
-     * @param array<array-key, mixed> $array
-     * @param array-key[] $keys
-     * @return array
+     * @template TArray of array<array-key, mixed>
+     *
+     * @param TArray $array
+     * @param array<int, array-key> $keys
+     * @return TArray
      */
     function array_only(array $array, array $keys): array
     {
@@ -18,7 +20,7 @@ if (! function_exists('array_only')) {
             $array,
             array_flip(array_unique(array_filter(
                 array_values($keys),
-                fn ($v) => is_int($v) || is_string($v),
+                fn ($v) => is_int($v) || is_string($v), // @phpstan-ignore function.alreadyNarrowedType
             )))
         );
     }
